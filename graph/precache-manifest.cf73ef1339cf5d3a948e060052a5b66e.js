4392675dd6068d541b4e8a8abbfa92d5ebd46698
Robin Kahlow <xtremegosugaming@googlemail.com>
2019-08-03 14:24:48 +0100

    Add files via upload

---
+++ graph/precache-manifest.cf73ef1339cf5d3a948e060052a5b66e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed22340de0068ea3ffd0d1d2db29c9a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6aed4eb1e7f87a81f13f",
+    "url": "/static/css/2.677f2dd4.chunk.css"
+  },
+  {
+    "revision": "9ff6ac8372b3a848e017",
+    "url": "/static/css/main.d83648f2.chunk.css"
+  },
+  {
+    "revision": "6aed4eb1e7f87a81f13f",
+    "url": "/static/js/2.dc8b6ea7.chunk.js"
+  },
+  {
+    "revision": "9ff6ac8372b3a848e017",
+    "url": "/static/js/main.bccfcf67.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
