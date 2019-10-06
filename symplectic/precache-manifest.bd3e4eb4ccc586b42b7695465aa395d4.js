35cb6fd62ca98e274e524377d961b6101a8c1ed0
Robin Kahlow <robinkahlow@web.de>
2019-10-06 15:14:55 +0100

    added symplectic

---
+++ symplectic/precache-manifest.bd3e4eb4ccc586b42b7695465aa395d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62724e4d793830f46d99511aa504aa13",
+    "url": "/symplectic/index.html"
+  },
+  {
+    "revision": "be49e97c56a8bc292200",
+    "url": "/symplectic/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "be49e97c56a8bc292200",
+    "url": "/symplectic/static/js/2.0b829028.chunk.js"
+  },
+  {
+    "revision": "28717d7c7e8a4924f453",
+    "url": "/symplectic/static/js/main.a10f785d.chunk.js"
+  },
+  {
+    "revision": "aa42a6ac910a130280d0",
+    "url": "/symplectic/static/js/runtime-main.0d0010ad.js"
+  }
+]);
