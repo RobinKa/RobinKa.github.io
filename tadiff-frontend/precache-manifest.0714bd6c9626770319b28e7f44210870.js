a8bb6d3f06c1b870af643038b3463b7f99f08a03
Robin Kahlow <robinkahlow@web.de>
2019-10-07 15:44:25 +0100

    added tadiff frontend

---
+++ tadiff-frontend/precache-manifest.0714bd6c9626770319b28e7f44210870.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39c43ce6cdccc9571d8fc46d82b0e6dc",
+    "url": "/tadiff-frontend/index.html"
+  },
+  {
+    "revision": "89b3b63f853c425118af",
+    "url": "/tadiff-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "fc79aa8917a152e57faa",
+    "url": "/tadiff-frontend/static/js/2.c774b5db.chunk.js"
+  },
+  {
+    "revision": "89b3b63f853c425118af",
+    "url": "/tadiff-frontend/static/js/main.58813558.chunk.js"
+  },
+  {
+    "revision": "d455c55f1d8e3db0d654",
+    "url": "/tadiff-frontend/static/js/runtime-main.ab1a1080.js"
+  }
+]);
