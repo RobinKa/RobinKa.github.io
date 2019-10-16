e7bc6e3d4034a11927b798058bdc04e84579286c
Robin Kahlow <robinkahlow@web.de>
2019-10-16 22:15:58 +0100

    added em sim

---
+++ maxwell/precache-manifest.e0a35454ff4966b92c47a1b1082d7f71.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15388a3558b00fff76c157d99733d568",
+    "url": "/maxwell/index.html"
+  },
+  {
+    "revision": "e668c588c4b87a63ca75",
+    "url": "/maxwell/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f51ce568ab534737cef2",
+    "url": "/maxwell/static/js/2.b0d5b994.chunk.js"
+  },
+  {
+    "revision": "e668c588c4b87a63ca75",
+    "url": "/maxwell/static/js/main.47844794.chunk.js"
+  },
+  {
+    "revision": "f4c4b6f5eb72283e1e2b",
+    "url": "/maxwell/static/js/runtime-main.993d012e.js"
+  }
+]);
